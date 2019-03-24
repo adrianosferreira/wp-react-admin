6d906d2de0a86a1230e80f0b8f1ddf769d2876db
Adriano Ferreira <adriano.f@onthegosystems.com>
2019-03-24 13:32:33 -0300

    Introduced admin notice component

---
+++ src/AdminNotice.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+
+class AdminNotice extends Component {
+
+	state = {
+		opacity: 1
+	};
+
+	decreaseOpacity() {
+		setTimeout(() => {
+			this.setState({opacity: this.state.opacity - 0.1});
+
+			if ( this.state.opacity > 0 ) {
+				this.decreaseOpacity();
+			}
+		}, 50)
+	}
+
+	componentDidMount() {
+		if ( this.props.disappear ) {
+			setTimeout(() => {
+				this.decreaseOpacity();
+			}, 3000);
+		}
+	}
+
+	render() {
+		let noticeClass = '';
+
+		switch ( this.props.type ) {
+			case 'error':
+				noticeClass = 'notice notice-error inline';
+				break;
+
+			case 'warning':
+				noticeClass = 'notice notice-warning inline';
+				break;
+
+			case 'info':
+				noticeClass = 'notice notice-info inline';
+				break;
+
+			default:
+				noticeClass = 'notice notice-success inline'
+				break;
+		}
+
+		if ( this.props.dismissible ) {
+			noticeClass += ' is-dismissible';
+		}
+
+		const style = {
+			opacity: this.state.opacity,
+			display: this.state.opacity > 0 ? 'block' : 'none',
+		};
+
+		return <div className={noticeClass} style={style}>
+			<p>{this.props.message}</p>
+		</div>
+	}
+}
+
+export default AdminNotice;
---
+++ tests/AdminNotice.react.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Enzyme, {shallow, mount} from 'enzyme';
+import AdminNotice from "../src/AdminNotice";
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('Admin notice component is rendered', () => {
+	test('renders', () => {
+		const wrapper = shallow(<AdminNotice/>);
+
+		expect(wrapper.exists()).toBe(true);
+	});
+
+	test('error class is rendered', () => {
+		const wrapper = shallow(<AdminNotice type="error"/>);
+
+		expect(wrapper.find('.notice')).toHaveLength(1);
+		expect(wrapper.find('.notice-error')).toHaveLength(1);
+		expect(wrapper.find('.inline')).toHaveLength(1);
+	});
+
+	test('info class is rendered', () => {
+		const wrapper = shallow(<AdminNotice type="info"/>);
+
+		expect(wrapper.find('.notice')).toHaveLength(1);
+		expect(wrapper.find('.notice-info')).toHaveLength(1);
+		expect(wrapper.find('.inline')).toHaveLength(1);
+	});
+
+	test('warning class is rendered', () => {
+		const wrapper = shallow(<AdminNotice type="warning"/>);
+
+		expect(wrapper.find('.notice')).toHaveLength(1);
+		expect(wrapper.find('.notice-warning')).toHaveLength(1);
+		expect(wrapper.find('.inline')).toHaveLength(1);
+	});
+
+	test('success class is rendered', () => {
+		const wrapper = shallow(<AdminNotice />);
+
+		expect(wrapper.find('.notice')).toHaveLength(1);
+		expect(wrapper.find('.notice-success')).toHaveLength(1);
+		expect(wrapper.find('.inline')).toHaveLength(1);
+	});
+
+	test('dismissible class is rendered', () => {
+		const wrapper = shallow(<AdminNotice dismissible={true}/>);
+
+		expect(wrapper.find('.notice')).toHaveLength(1);
+		expect(wrapper.find('.notice-success')).toHaveLength(1);
+		expect(wrapper.find('.inline')).toHaveLength(1);
+		expect(wrapper.find('.is-dismissible')).toHaveLength(1);
+	});
+});
